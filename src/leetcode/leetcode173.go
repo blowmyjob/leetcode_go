e7cab754fda583fcf78bfed211867ec47f65440b
1026847645@qq.com <1026847645@qq.com>
2021-07-05 22:42:48 +0800

    1

---
+++ src/leetcode/leetcode173.go
@@ -0,0 +1,29 @@
+package leetcode
+
+type BSTIterator struct {
+	arr []int
+}
+
+func Constructor(root *TreeNode) (it BSTIterator) {
+	it.inOrder(root)
+	return
+}
+
+func (this *BSTIterator) Next() int {
+	val := this.arr[0]
+	this.arr = this.arr[1:]
+	return val
+}
+
+func (this *BSTIterator) HasNext() bool {
+	return len(this.arr) > 0
+}
+
+func (this *BSTIterator) inOrder(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	this.inOrder(root.Left)
+	this.arr = append(this.arr, root.Val)
+	this.inOrder(root.Right)
+}
